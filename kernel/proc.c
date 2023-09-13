672a33dddf820033ab6cd74c494cc013d6818de6
FKK1017 <danteeee0511@gmail.com>
2023-09-14 00:19:12 +0800

    20230913, Speed up System Call

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,16 @@ found:
     return 0;
   }
 
+  #ifdef LAB_PGTBL
+  if ((p->pidMap = (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->pidMap->pid = p->pid;
+  //printf("%d\n", p->pidMap->pid);
+  #endif
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -158,6 +168,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->pidMap)
+    kfree((void*)p->pidMap);
+  p->pidMap = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -202,6 +215,16 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  #ifdef LAB_PGTBL
+  if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->pidMap), PTE_R | PTE_U) < 0)
+  {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+  #endif
+
   return pagetable;
 }
 
@@ -212,6 +235,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  #ifdef LAB_PGTBL
+  uvmunmap(pagetable, USYSCALL, 1, 0);
+  #endif
   uvmfree(pagetable, sz);
 }
 
