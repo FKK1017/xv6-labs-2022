32dd4f720d0b3cb460dfa0d30a33723cfd3e0928
FKK1017 <danteeee0511@gmail.com>
2023-09-21 20:15:39 +0800

    Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,46 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_helper(pagetable_t pagetable, int depth) {
+  for (int i=0; i<512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      for (int j=0; j<depth; j++) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      vmprint_helper((pagetable_t)PTE2PA(pte), depth+1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pgtblt)
+{
+  printf("page table %p\n", pgtblt);
+  vmprint_helper(pgtblt, 1);
+}
+
+
+
+/*
+void
+freewalk(pagetable_t pagetable)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      freewalk((pagetable_t)child);
+      pagetable[i] = 0;
+    } else if(pte & PTE_V){
+      panic("freewalk: leaf");
+    }
+  }
+  kfree((void*)pagetable);
+}
+*/
